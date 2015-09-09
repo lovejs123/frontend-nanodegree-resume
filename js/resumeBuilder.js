d0447f52333a153c6ffb8d5662e3627e96170852
Tony James <tony.james@cohaesus.co.uk>
2015-09-09 17:40:56 +0100

    Fix 2 Fun

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,6 @@
 $("#main").append(["Tony James"]);
 var awesomeThaughts = "I am Tony and i am AWESOME!";
 console.log(awesomeThaughts);
-awesomeThaughts.replace("AWESOME", "fun");
-console.log(awesomeThaughts);
+var funThaughts =
+    awesomeThaughts.replace("AWESOME", "fun");
+console.log(funThaughts);
